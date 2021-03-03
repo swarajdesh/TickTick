4b176aee40ed3219814a2e08a30d6d491593be48
swaraj <swarajdeshmukh45@gmail.com>
2021-03-03 12:25:09 +0530

    Combining Multiple Flows

--- app/src/main/java/com/example/ticktick/data/TaskDao.kt
+++ app/src/main/java/com/example/ticktick/data/TaskDao.kt
@@ -1,14 +1,25 @@
 package com.example.ticktick.data
 
 import androidx.room.*
+import com.example.ticktick.UI.tasks.SortOrder
 import kotlinx.coroutines.flow.Flow
 
 @Dao  //Data Access Objects (Dao) are the main classes where you define your database interactions. They can include a variety of query methods.
 interface TaskDao {
 
-    @Query("SELECT * FROM task_table WHERE name LIKE '%' || :searchQuery || '%' ORDER BY important DESC")
+    fun getTasks(query: String, sortOrder: SortOrder, hideCompleted: Boolean): Flow<List<Task>> =
+        when(sortOrder) {
+            SortOrder.BY_DATE -> getTasksSortedByDateCreated(query, hideCompleted)
+            SortOrder.BY_NAME -> getTasksSortedByName(query, hideCompleted)
+
+        }
+
+    @Query("SELECT * FROM task_table WHERE (completed != :hideCompleted OR completed = 0) AND name LIKE '%' || :searchQuery || '%' ORDER BY important DESC, name")
     fun getTasksSortedByName(searchQuery: String, hideCompleted: Boolean): Flow<List<Task>>  // Flow represents stream of data and Flow can only be used inside the coroutines that is why we don't need suspend modifier
     //Flow is asynchronous stream of data
+
+    @Query("SELECT * FROM task_table WHERE (completed != :hideCompleted OR completed = 0) AND name LIKE '%' || :searchQuery || '%' ORDER BY important DESC, created")
+    fun getTasksSortedByDateCreated(searchQuery: String, hideCompleted: Boolean): Flow<List<Task>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(task: Task) // creates new thread for room suspend function
