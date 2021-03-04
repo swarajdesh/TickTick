3e50076775f37874d44fd5e77ca9ff40d1057fe9
swaraj <swarajdeshmukh45@gmail.com>
2021-03-05 00:41:18 +0530

    Saving Data with Jetpack DataStore

--- app/src/main/java/com/example/ticktick/UI/tasks/TaskFragment.kt
+++ app/src/main/java/com/example/ticktick/UI/tasks/TaskFragment.kt
@@ -8,12 +8,18 @@ import android.view.View
 import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.ticktick.R
+import com.example.ticktick.data.SortOrder
 import com.example.ticktick.databinding.FragmentTasksBinding
 import com.example.ticktick.util.onQueryTextChanged
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+
 /**
 Author : Swaraj Deshmukh
  **/
@@ -64,21 +70,26 @@ class TaskFragment : Fragment(R.layout.fragment_tasks) {
             /**
              It bascically runs a new sqlite query that returns a new flow whenever we type a new letter */
          }
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            menu.findItem(R.id.action_hide_completed_tasks).isChecked =
+                viewModel.preferencesFlow.first().hideCompleted
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
        return when(item.itemId) {
             R.id.action_sort_by_name -> {
-                viewModel.sortOrder.value = SortOrder.BY_NAME
+                viewModel.onSortOrderSelected(SortOrder.BY_NAME)
                 true
             }
            R.id.action_sort_by_date_created ->{
-                viewModel.sortOrder.value = SortOrder.BY_DATE
+                viewModel.onSortOrderSelected(SortOrder.BY_DATE)
                true
            }
            R.id.action_hide_completed_tasks -> {
                item.isChecked = !item.isChecked
-               viewModel.hideCompleted.value = item.isChecked
+               viewModel.onHideCompletedClick(item.isChecked)
                true
            }
            R.id.action_delete_all_completed_tasks -> {
